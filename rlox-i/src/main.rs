4363834f6313ee856fe73e21af796fb20c5e6bf6
pedropiin <pedarosa04@gmail.com>
2025-12-15 02:41:03 -0300

    Beginning implementation of Part I: Tree-Walk Interpreter

--- rlox-i/src/main.rs
+++ rlox-i/src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::process;
+
+
+enum LexerErrors {
+    UsageError,
+}
+
+impl LexerErrors {
+    fn message(&self) -> &str {
+        match self {
+            LexerErrors::UsageError => "Usage: rlox [filename.lox].",
+        }
+    }
+}
+
+fn run_file(path: &String) {
+    todo!();
+}
+
+fn run_prompt() {
+    todo!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        eprintln!("{}", LexerErrors::UsageError.message());
+        process::exit(64);      // following UNIX "sysexits.h" header convention
+    } else if args.len() == 1 {
+        run_file(&args[0]);
+    } else {
+        run_prompt();
+    }
 }
