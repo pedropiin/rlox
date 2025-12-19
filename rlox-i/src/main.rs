0ee9b4a98e84d9cc5e219a3880a2b56a14fe759a
pedropiin <pedarosa04@gmail.com>
2025-12-18 21:42:27 -0300

    (1) Refactoring files and structs into separate modules and crates. (2) Opting out of the whole 'lifetime' strategy for lexemes and using indices for string slices instead

--- rlox-i/src/main.rs
+++ rlox-i/src/main.rs
@@ -2,60 +2,21 @@ use std::env;
 use std::process;
 use std::fs;
 use std::io;
-use std::fmt;
 
-#[derive(Debug)]
-enum TokenType {
-    LeftParen,
-    RightParen, 
-    LeftBrace, 
-    RightBrace, 
-    Comma, 
-    Dot, 
-    Minus, 
-    Plus, 
-    Semicolon, 
-    Slash, 
-    Star,
-    Bang, 
-    BangEqual, 
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-    Identifier,
-    String,
-    Number,
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
-    Eof,
-}
+use crate::token::Token;
+use crate::scanner::Scanner;
+
+mod token;
+mod scanner;
+mod token_type;
 
 enum LexerErrors {
-    UsageError,
     SourceReadError,
 }
 
 impl LexerErrors {
     fn message(&self) -> &str {
         match self {
-            LexerErrors::UsageError     => "Usage: rlox [filename.lox].",
             LexerErrors::SourceReadError  => "Could not read/open source lox file.",
         }
     }
@@ -69,42 +30,8 @@ fn error(line: usize, error_type: LexerErrors) {
     report(line, "", error_type);
 }
 
-struct Token<'b> {
-    token_type: TokenType,
-    lexeme: &'b str,
-    line: usize,
-}
-
-impl<'b> Token<'b> {
-    fn new(token_type: TokenType, lexeme: &'b str, line: usize) -> Self {
-        Token { token_type, lexeme, line }
-    }
-}
-
-impl<'b> fmt::Display for Token<'b> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<{:?}, {}, {}>", self.token_type, self.lexeme, self.line)
-    }
-}
-
-struct Scanner<'a> {
-    source: &'a str,
-    tokens: Vec<Token<'a>>,
-}
-
-impl<'a> Scanner<'a> {
-    fn new(source: &'a str) -> Self {
-        let mut tokens: Vec<Token> = Vec::new();
-        Scanner { source, tokens }
-    }
-
-    fn scan_tokens(&self) -> Vec<Token> {
-        todo!();
-    }
-}
-
 fn run(source: &str) {
-    let scanner: Scanner = Scanner::new(source);
+    let mut scanner: Scanner = Scanner::new(source);
     let tokens: Vec<Token> = scanner.scan_tokens(); 
 
     for tok in tokens {
@@ -129,7 +56,7 @@ fn run_prompt() {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        eprintln!("{}", LexerErrors::UsageError.message());
+        eprintln!("Usage: rlox [filename.lox].");
         process::exit(64);      // following UNIX "sysexits.h" header convention
     } else if args.len() == 1 {
         run_file(&args[0]);
